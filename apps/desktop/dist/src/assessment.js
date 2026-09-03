a257947a44ced427684380e6d6f304b3dc71abd8
Ivan Fraixedes <ivan.fraixedes@suse.com>
2026-09-03 16:19:29 +0200

    Ignore dist folders & delete the current commited sources

--- apps/desktop/dist/src/assessment.js
+++
@@ -1,3 +0,0 @@
-import { runDesktop } from './start.js';
-import { ASSESSMENT_TARGET } from './target.js';
-runDesktop(ASSESSMENT_TARGET);
--- apps/desktop/dist/src/author.js
+++
@@ -1,3 +0,0 @@
-import { runDesktop } from './start.js';
-import { AUTHOR_TARGET } from './target.js';
-runDesktop(AUTHOR_TARGET);
