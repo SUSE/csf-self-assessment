05eef4ed6b9a24e110a02f248b4916ca2c11bf64
Ivan Fraixedes <ivan.fraixedes@suse.com>
2026-09-09 22:21:54 +0200

    tidy samples only for test and imports

--- packages/platform/src/test-fixtures.ts
+++ packages/platform/src/test-fixtures.ts
@@ -1,21 +1,20 @@
-import partialAlexJson from '../../../samples/recommendations/partial-Alex.json' with { type: 'json' };
-import partialJaneJson from '../../../samples/recommendations/partial-Jane.json' with { type: 'json' };
-import workbookAssessmentJson from '../../../samples/recommendations/workbook-assessment.json' with { type: 'json' };
-import workbookJson from '../../../samples/recommendations/workbook.json' with { type: 'json' };
-
-import csfWorkbookJson from '../../../samples/csf-workbook.json' with { type: 'json' };
 import euCsfCalculatorDeepFillAlexJson from '../../../samples/ec-guidance-deep-analysis/alex.json' with { type: 'json' };
 import euCsfCalculatorDeepFillJaneJson from '../../../samples/ec-guidance-deep-analysis/jane.json' with { type: 'json' };
 import euCsfCalculatorDeepWorkbookJson from '../../../samples/ec-guidance-deep-analysis/workbook.json' with { type: 'json' };
 import euCsfCalculatorFillAlexJson from '../../../samples/ec-guidance-complete/alex.json' with { type: 'json' };
 import euCsfCalculatorFillJaneJson from '../../../samples/ec-guidance-complete/jane.json' with { type: 'json' };
 import euCsfCalculatorWorkbookJson from '../../../samples/ec-guidance-complete/workbook.json' with { type: 'json' };
-import sampleWorkbookJson from '../../../samples/sample-workbook.json' with { type: 'json' };
 import teachingDeepAnalysisAssessmentJson from '../../../samples/learn-deep-analysis/assessment.json' with { type: 'json' };
 import teachingDeepAnalysisWorkbookAssessmentJson from '../../../samples/learn-deep-analysis/workbook-assessment.json' with { type: 'json' };
 import teachingDeepAnalysisWorkbookJson from '../../../samples/learn-deep-analysis/workbook.json' with { type: 'json' };
 import teachingWorkbookAssessmentJson from '../../../samples/learn-basics/workbook-assessment.json' with { type: 'json' };
 import teachingWorkbookJson from '../../../samples/learn-basics/workbook.json' with { type: 'json' };
+import partialAlexJson from '../../../samples/recommendations/partial-Alex.json' with { type: 'json' };
+import partialJaneJson from '../../../samples/recommendations/partial-Jane.json' with { type: 'json' };
+import workbookAssessmentJson from '../../../samples/recommendations/workbook-assessment.json' with { type: 'json' };
+import workbookJson from '../../../samples/recommendations/workbook.json' with { type: 'json' };
+import csfWorkbookJson from '../../../samples/test-fixtures/csf-workbook.json' with { type: 'json' };
+import sampleWorkbookJson from '../../../samples/test-fixtures/sample-workbook.json' with { type: 'json' };
 
 export const alexRaw = partialAlexJson;
 export const janeRaw = partialJaneJson;
